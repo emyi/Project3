82c6bdf32aece4966c555284b181639a079c7c93
Eric Yi <trend1987@gmail.com>
2015-10-02 17:35:19 -0700

    add course model

--- project3/models/course.js
+++ project3/models/course.js
@@ -1,1 +1,11 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var courseSchema = mongoose.Schema({
+	name: String,
+	address: String,
+	price: Number
+});
+
+var Course = mongoose.model('Course', courseSchema);
+module.exports = Course;
