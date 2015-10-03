8617cbfad70015d199e16e94679ed4dea73a023f
Eric Yi <trend1987@gmail.com>
2015-10-03 15:28:39 -0700

    work on users controller

--- project3/routes/users.js
+++ project3/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var router = express.Router();
+var usersController = express.Router();
+var User = require('../models/user.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
