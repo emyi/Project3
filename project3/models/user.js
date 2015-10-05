6f30ced03957c892bab9a88a32555ec7273c4f4a
emyi <trend1987@gmail.com>
2015-10-05 11:59:44 -0700

    work on user model to authenticate

--- project3/models/user.js
+++ project3/models/user.js
@@ -10,5 +10,25 @@ var userSchema = mongoose.Schema({
 	handicap: Number
 });
 
+userSchema.pre('save',function (next){
+   var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+   return bcrypt.genSaltAsync(10).then(function(result) {
+
+        return bcrypt.hashAsync(user.password, result).then(function(hash){
+            console.log("hash: " + hash);
+            user.password = hash;
+            next();
+        });
+   })
+});
+
+userSchema.methods.comparePasswordAsync = function(candidatePassword) {
+    return bcrypt.compareAsync(candidatePassword, this.password);
+};
+
 var User = mongoose.model('User', userSchema);
 module.exports = User;
