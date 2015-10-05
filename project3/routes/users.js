741ff3930cc82df51b3ab8a9da65382255e9a636
emyi <trend1987@gmail.com>
2015-10-05 16:31:02 -0700

    fixed users controller to get users index

--- project3/routes/users.js
+++ project3/routes/users.js
@@ -3,20 +3,22 @@ var usersController = express.Router();
 var User = require('../models/user.js');
 
 usersController.get('/users', function (req, res){
-	if(req.session && req.session.email){
-		User.findOne({ email: req.session.email}).then(function(user, err){
-			console.log(users.length);
-			res.render('users/index.ejs', {
-				users: users,
-				curr_user: user.email
+	User.findAsync({}).then(function (users, err){
+		if(req.session && req.session.email){
+			User.findOne({ email: req.session.email}).then(function(user, err){
+				console.log(users.length);
+				res.render('users/index.ejs', {
+					users: users,
+					curr_user: user.email
+				});
 			});
-		});
-	}else{
-		res.render('users/index.ejs', {
-			curr_user: null,
-			users: users
-		});
-	}
+		}else{
+			res.render('users/index.ejs', {
+				curr_user: null,
+				users: users
+			});
+		}
+	});
 });
 
 usersController.get('/users/:id', function (req, res){
